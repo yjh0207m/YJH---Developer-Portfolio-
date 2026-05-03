303f7736ec2ccbd92a977a97899c19f2b3d81a4d
johyun <yjh0207m@naver.com>
2026-05-03 16:35:23 +0900

    fix: make JavaMailSender optional to allow startup without mail config

--- portfolio-back/src/main/java/com/yjh/portfolio/service/ContactService.java
+++ portfolio-back/src/main/java/com/yjh/portfolio/service/ContactService.java
@@ -1,6 +1,7 @@
 package com.yjh.portfolio.service;
 
 import com.yjh.portfolio.dto.ContactDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,16 +10,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ContactService {
 
-    private final JavaMailSender mailSender;
+    @Autowired(required = false)
+    private JavaMailSender mailSender;
 
     @Value("${mail.to:yjh0207m@naver.com}")
     private String mailTo;
 
-    public ContactService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
     public void send(ContactDto dto) {
+        if (mailSender == null) {
+            throw new IllegalStateException("메일 서버가 설정되지 않았습니다.");
+        }
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(mailTo);
         msg.setSubject("[포트폴리오 문의] " + dto.name());
